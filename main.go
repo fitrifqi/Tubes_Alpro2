d664f1c781944d6d53e453483b1079ccec637c48
fitrifqi <rifqibocilgeming2008@gmail.com>
2026-06-17 20:54:45 +0700

    last revision

--- main.go
+++ main.go
@@ -360,10 +360,11 @@ func showStatistics(d tabData, n int) {
 		fmt.Println("No data available!")
 		return
 	}
-	var total, total1, besar int
+	var total, total1, besar, idxbesar int
 	for i := 0; i < n; i++ {
 		if i == 0 || (besar < (d[i].watt * d[i].time)) {
 			besar = d[i].watt * d[i].time
+			idxbesar = i
 		}
 		total += d[i].time
 		total1 += d[i].watt * d[i].time
@@ -372,5 +373,5 @@ func showStatistics(d tabData, n int) {
 	fmt.Println("Total Usage Time :", total)
 	fmt.Println("Average Usage Time :", float64(total)/float64(n))
 	fmt.Println("Average Energy Consumed :", float64(total1)/float64(n))
-	fmt.Println("Device with Most Energy Consumed : ", besar)
+	fmt.Println("Device with Most Energy Consumed : ", d[idxbesar].name, "With Amount of Energy : ", besar)
 }
